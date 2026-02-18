d60c6cefdc739f1a7ca3559b08556964dd8bab64
Sudhanshu Ranjan <sudhanshuranjan9608@gmail.com>
2026-02-18 13:29:44 +0530

    day 1 of Learning Backend

---
+++ DAY1/src/app.js
@@ -0,0 +1,34 @@
+// server ko create karna
+
+const express = require('express');
+
+
+ const app = express()
+
+
+
+ /*
+
+ note ={
+ title:'my first note',
+ description:'this is my first note',
+ 
+ }
+
+ const notes =[
+ {
+    title:'my first note',
+    description:'this is my first note'
+    },
+
+    {
+        title:'my second note',
+        description:'this is my second note'
+    },
+
+ ]
+
+
+ */
+
+ module.exports = app;
