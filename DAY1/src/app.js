b41917f39ff7f224a8939f5c94c09c9263652a54
Sudhanshu Ranjan <sudhanshuranjan9608@gmail.com>
2026-02-23 16:19:16 +0530

    day 1 completed

--- DAY1/src/app.js
+++ DAY1/src/app.js
@@ -4,31 +4,56 @@ const express = require('express');
 
 
  const app = express()
+ app.use(express.json()) // json data ko read krne ke liye middleware use krna padta hai
+
+ const notes = []
+
+ /* title , description */
 
 
+ //post /notes/
+ app.post('/notes',(req,res)=>{
+    notes.push(req.body)
 
- /*
+    res.status(201).json({
+        message : "note created successfully",
+    })
 
- note ={
- title:'my first note',
- description:'this is my first note',
- 
- }
+ }) 
 
- const notes =[
- {
-    title:'my first note',
-    description:'this is my first note'
-    },
+ /* GET /notes */
+ app.get('/notes',(req,res)=>{
+    
+    res.status(200).json({
+        message : "notes fetched successfully",
+        notes : notes
+    })
+ })
 
-    {
-        title:'my second note',
-        description:'this is my second note'
-    },
+ /* delete /notes/:index */
+ app.delete('/notes/:index',(req,res)=>{
+    const index = req.params.index
+    
+    delete notes[index]
 
- ]
+    res.status(200).json({
+        message : "note deleted successfully",
+    })
 
+ }) 
 
- */
+ app.patch('/notes/:index',(req,res) =>{
+
+    const index = req.params.index
+    const description = req.body.description 
+
+    notes[index].description = description
+
+    res.status(200).json({
+        message : "note updated successfully",
+    })
+
+    
+ })
 
  module.exports = app;
