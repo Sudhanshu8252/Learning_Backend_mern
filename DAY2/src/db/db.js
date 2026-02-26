6a5f414be02fd3887f2f9f3206ed820b0acf51ca
Sudhanshu Ranjan <sudhanshuranjan9608@gmail.com>
2026-02-26 14:09:39 +0530

    Day  2 completed

--- DAY2/src/db/db.js
+++ DAY2/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 async function connectDB(){
-    await mongoose.connect("mongodb+srv://yt:JiGbemyuASipeAMX@yt-complete-backend.kzsrvyu.mongodb.net/halley")
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("Connected to DB");
 }
 
